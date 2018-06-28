5f5f5484b868d8b201755f0bd36b58d723db61fb
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-06-28 12:39:59 +0200

    Added Aux files from Prob Depth Project

---
+++ ImageProcessing/Utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def z_standardise(pixels):
+    mean = np.mean(pixels)
+    std_dev = np.std(pixels)
+
+    return (pixels - mean) / std_dev
+
+
+def normalize_to_image_space(pixels):
+    pixels_min = np.min(pixels)
+    pixels_max = np.max(pixels)
+
+    # https://en.wikipedia.org/wiki/Normalization_(image_processing)
+    mapped_float = (pixels - pixels_min) * (255.0 / (pixels_max - pixels_min))
+    return mapped_float.astype(np.uint8)
