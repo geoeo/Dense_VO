0f655d66525f72cf2a7878ef43f2c9dd0b94bcb6
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2018-07-17 14:18:55 +0200

    added convolution option to frame class

--- Frame.py
+++ Frame.py
@@ -22,3 +22,21 @@ class Frame:
     def scale_frame_by(self,scale_factor):
         self.pixel_image = cv2.resize(self.pixel_image, (0, 0), fx=scale_factor, fy=scale_factor)
         self.camera = self.camera.intrinsic.scale_by(scale_factor)
+
+    def convolve_frame_by(self,scale_factor):
+        (rows,cols) = self.pixel_image.size
+        dst_size = (int(scale_factor * rows), int(scale_factor * cols))
+        dst_size_flipped = (int(scale_factor * cols), int(scale_factor * rows))
+        output_image = np.zeros(dst_size)
+        cv2.pyrDown(self.pixel_image,output_image,dst_size_flipped)
+        self.pixel_image = output_image
+        self.camera = self.camera.intrinsic.scale_by(scale_factor)
+
+    #TODO: TEST
+    # Assumes that image has been down sampled with 2x2 image blocks i.e scale = 0.5
+    def reconstruct_depth(self,x_low,y_low,layers_between):
+        two_l = 2**layers_between
+        two_l_minus_one = 0.5*(two_l - 1)
+        x_high = two_l*x_low + two_l_minus_one
+        y_high = two_l*y_low + two_l_minus_one
+        return x_high , y_high
--- ImageLoader.py
+++ ImageLoader.py
@@ -16,6 +16,39 @@ level_3 = cv2.resize(level_2, (0,0), fx=0.5, fy=0.5)
 level_4 = cv2.resize(level_3, (0,0), fx=0.5, fy=0.5)
 level_5 = cv2.resize(level_4, (0,0), fx=0.5, fy=0.5)
 
+(y_1,x_1) = pixels_standardised.shape
+dst_size_level_1 = (int(0.5*y_1),int(0.5*x_1))
+dst_size_level_1_flipped = (int(0.5*x_1),int(0.5*y_1))
+output_image_level_1 = np.zeros(dst_size_level_1)
+
+(y_2,x_2) = output_image_level_1.shape
+dst_size_level_2 = (int(0.5*y_2),int(0.5*x_2))
+dst_size_level_2_flipped = (int(0.5*x_2),int(0.5*y_2))
+output_image_level_2 = np.zeros(dst_size_level_2)
+
+(y_3,x_3) = output_image_level_2.shape
+dst_size_level_3 = (int(0.5*y_3),int(0.5*x_3))
+dst_size_level_3_flipped = (int(0.5*x_3),int(0.5*y_3))
+output_image_level_3 = np.zeros(dst_size_level_3)
+
+(y_4,x_4) = output_image_level_3.shape
+dst_size_level_4 = (int(0.5*y_4),int(0.5*x_4))
+dst_size_level_4_flipped = (int(0.5*x_4),int(0.5*y_4))
+output_image_level_4 = np.zeros(dst_size_level_4)
+
+(y_5,x_5) = output_image_level_4.shape
+dst_size_level_5 = (int(0.5*y_5),int(0.5*x_5))
+dst_size_level_5_flipped = (int(0.5*x_5),int(0.5*y_5))
+output_image_level_5 = np.zeros(dst_size_level_5)
+
+
+cv2.pyrDown(pixels_standardised,output_image_level_1,dst_size_level_1_flipped)
+cv2.pyrDown(output_image_level_1,output_image_level_2,dst_size_level_2_flipped)
+cv2.pyrDown(output_image_level_2,output_image_level_3,dst_size_level_3_flipped)
+cv2.pyrDown(output_image_level_3,output_image_level_4,dst_size_level_4_flipped)
+cv2.pyrDown(output_image_level_4,output_image_level_5,dst_size_level_5_flipped)
+
+
 print(im_greyscale.shape)
 print(level_1.shape)
 print(level_2.shape)
@@ -23,13 +56,26 @@ print(level_3.shape)
 print(level_4.shape)
 print(level_5.shape)
 
+print(output_image_level_1.shape)
+print(output_image_level_2.shape)
+print(output_image_level_3.shape)
+print(output_image_level_4.shape)
+print(output_image_level_5.shape)
+
 cv2.imshow('Level 0 (Norm to 255)',pixels_normalized_disp)
 cv2.imshow('Level 0',pixels_standardised)
+
 cv2.imshow('Level 1',level_1)
 cv2.imshow('Level 2',level_2)
 cv2.imshow('Level 3',level_3)
 cv2.imshow('Level 4',level_4)
 cv2.imshow('Level 5',level_5)
+
+cv2.imshow('Level 1 Conv',output_image_level_1)
+cv2.imshow('Level 2 Conv',output_image_level_2)
+cv2.imshow('Level 3 Conv',output_image_level_3)
+cv2.imshow('Level 4 Conv',output_image_level_4)
+cv2.imshow('Level 5 Conv',output_image_level_5)
 
 while True:
     k = cv2.waitKey(5) & 0xFF
