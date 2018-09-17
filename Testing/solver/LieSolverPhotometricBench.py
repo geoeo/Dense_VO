started vis on SE3 estimates; fixed error with variance weighting; fixed error with least squares (takes abs value?)

Testing/solver/LieSolverPhotometricBench.py
@@ -21,14 +21,18 @@ depth_text = dataset_root+'depth.txt'
 match_text = dataset_root+'matches.txt'
 groundtruth_text = dataset_root+'groundtruth.txt'
 
-rgb_id_ref = 1305031102.175304
-rgb_id_target = 1305031102.211214
+#rgb_id_ref = 1305031102.175304
+#rgb_id_target = 1305031102.211214
 
 #rgb_id_ref = 1305031108.743502
 #rgb_id_target = 1305031108.775493
 
 #rgb_id_ref = 1305031119.615017
 #rgb_id_target = 1305031119.647903
+
+rgb_id_ref = 1305031106.675279
+rgb_id_target = 1305031106.711508
+
 
 image_groundtruth_dict = dict(associate.match(rgb_text,groundtruth_text))
 
@@ -41,7 +45,6 @@ groundtruth_data_target = associate.return_groundtruth(groundtruth_text,groundtr
 SE3_target = Parser.generate_se3_from_groundtruth(groundtruth_data_target)
 
 SE3_ref_target = SE3.pose_pose_composition_inverse(SE3_ref,SE3_target)
-
 
 rgb_ref_file_path , depth_ref_file_path = associate.return_rgb_depth_from_rgb_selection(rgb_text,depth_text, match_text, dataset_root, rgb_id_ref)
 rgb_target_file_path , depth_target_file_path = associate.return_rgb_depth_from_rgb_selection(rgb_text,depth_text, match_text, dataset_root, rgb_id_target)
@@ -66,9 +69,9 @@ im_depth_target /= depth_factor
 
 se3_identity = np.identity(4, dtype=Utils.matrix_data_type)
 
-intrinsic_identity = Intrinsic.Intrinsic(1, 1, image_width/2, image_height/2)
+intrinsic_identity = Intrinsic.Intrinsic(-1, 1, image_width/2, image_height/2)
 if use_ndc:
-    intrinsic_identity = Intrinsic.Intrinsic(-1, -1, 1/2, 1/2) # for ndc
+    intrinsic_identity = Intrinsic.Intrinsic(-1, 1, 1/2, 1/2) # for ndc
 
 camera_reference = Camera.Camera(intrinsic_identity, se3_identity)
 camera_target = Camera.Camera(intrinsic_identity, se3_identity)
@@ -77,7 +80,15 @@ camera_target = Camera.Camera(intrinsic_identity, se3_identity)
 frame_reference = Frame.Frame(im_greyscale_reference, im_depth_reference, camera_reference, False)
 frame_target = Frame.Frame(im_greyscale_target, im_depth_target, camera_target, True)
 
-SE3_est = Solver.solve_photometric(frame_reference, frame_target, 20000, 0.1, alpha_step=0.01, use_ndc=use_ndc, debug = False)
+SE3_est = Solver.solve_photometric(frame_reference,
+                                   frame_target,
+                                   20000,
+                                   1.0,
+                                   alpha_step=1.0,
+                                   use_ndc=use_ndc,
+                                   use_robust = True,
+                                   debug = False)
+
 euler_angles_XYZ = SE3.rotationMatrixToEulerAngles(SE3.extract_rotation(SE3_est))
 
 print('*'*80)

Testing/solver/LieSolverPhotometricHomeTest.py
@@ -8,39 +8,25 @@ from VisualOdometry import Frame
 from Numerics import ImageProcessing
 from Numerics import SE3
 
-# TODO use raw depth values
 
-#im_greyscale_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_fov_90_square_Y.png',cv2.IMREAD_GRAYSCALE)
-#im_greyscale_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_fov_90_square.png',cv2.IMREAD_GRAYSCALE)
-#im_greyscale_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_fov_90_square_negative.png',cv2.IMREAD_GRAYSCALE)
 #im_greyscale_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_X_Trans_Depth_Aligned_Scaled/image_40.png',cv2.IMREAD_GRAYSCALE)
 im_greyscale_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Y_Trans_Depth_Aligned_Scaled/image_40.png',cv2.IMREAD_GRAYSCALE)
 #im_greyscale_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Board_X_Trans_Depth_Aligned_Scaled/image_126.png',cv2.IMREAD_GRAYSCALE)
 
 im_greyscale_reference = ImageProcessing.z_standardise(im_greyscale_reference)
 
-
-#im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_translated_fov_90_square_Y.png',cv2.IMREAD_GRAYSCALE)
-#im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_translated_fov_90_square.png',cv2.IMREAD_GRAYSCALE)
-#im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_left_90_fov_90_square.png',cv2.IMREAD_GRAYSCALE)
-#im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/framebuffer_translated_fov_90_square_negative.png',cv2.IMREAD_GRAYSCALE)
 #im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_X_Trans_Depth_Aligned_Scaled/image_41.png',cv2.IMREAD_GRAYSCALE)
 im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Y_Trans_Depth_Aligned_Scaled/image_41.png',cv2.IMREAD_GRAYSCALE)
 #im_greyscale_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Board_X_Trans_Depth_Aligned_Scaled/image_127.png',cv2.IMREAD_GRAYSCALE)
 
 im_greyscale_target = ImageProcessing.z_standardise(im_greyscale_target)
 
-#depth_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/depthbuffer_fov_90_square_Y.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type_int)
-#depth_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/depthbuffer_fov_90_square.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type)
-#depth_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/depthbuffer_fov_90_square_negative.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type)
 #depth_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_X_Trans_Depth_Aligned_Scaled/image_depth_aligned_40.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type_int)
 depth_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Y_Trans_Depth_Aligned_Scaled/image_depth_aligned_40.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type_int)
 #depth_reference = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Board_X_Trans_Depth_Aligned_Scaled/image_depth_aligned_126.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type)
 #depth_reference = ImageProcessing.z_standardise(depth_reference)
 
-#depth_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/depthbuffer_translated_fov_90_square_Y.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type_int)
-#depth_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/depthbuffer_translated_fov_90_square.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type)
-#depth_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Synthetic/depthbuffer_left_90_fov_90_square.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type)
+
 #depth_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_X_Trans_Depth_Aligned_Scaled/image_depth_aligned_41.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type_int)
 depth_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Y_Trans_Depth_Aligned_Scaled/image_depth_aligned_41.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type_int)
 #depth_target = cv2.imread('/Users/marchaubenstock/Workspace/Diplomarbeit_Resources/VO_Home_Images/Images_ZR300_Board_X_Trans_Depth_Aligned_Scaled/image_depth_aligned_127.png',cv2.IMREAD_ANYDEPTH).astype(Utils.depth_data_type)
@@ -58,9 +44,10 @@ use_ndc = True
 se3_identity = np.identity(4, dtype=Utils.matrix_data_type)
 
 # fx and fy affect the resulting coordiante system of the se3 matrix
-intrinsic_identity = Intrinsic.Intrinsic(-1, -1, image_width/2, image_height/2)
+intrinsic_identity = Intrinsic.Intrinsic(-1, 1, image_width/2, image_height/2)
 if use_ndc:
-    intrinsic_identity = Intrinsic.Intrinsic(-1, -1, 1/2, 1/2) # for ndc
+    intrinsic_identity = Intrinsic.Intrinsic(-1, 1, 1/2, 1/2) # for ndc
+
 
 
 # reference frame is assumed to be the origin
@@ -72,8 +59,14 @@ camera_target = Camera.Camera(intrinsic_identity, se3_identity)
 frame_reference = Frame.Frame(im_greyscale_reference, depth_reference, camera_reference, False)
 frame_target = Frame.Frame(im_greyscale_target, depth_target, camera_target, True)
 
-#SE3_est = Solver.solve_photometric(frame_reference, frame_target, 20000, 0.03, alpha_step=1.0, use_ndc = use_ndc, debug = False)# - Synth Y
-SE3_est = Solver.solve_photometric(frame_reference, frame_target, 20000, 1.0, alpha_step=0.5, use_ndc = use_ndc, debug = False)
+SE3_est = Solver.solve_photometric(frame_reference,
+                                   frame_target,
+                                   20000,
+                                   0.2,
+                                   alpha_step=1.0,
+                                   use_ndc = use_ndc,
+                                   use_robust = False,
+                                   debug = False)
 euler_angles_XYZ = SE3.rotationMatrixToEulerAngles(SE3.extract_rotation(SE3_est))
 
 print(SE3_est)

Testing/visualization/VisualizeRotatePlaneTest.py
@@ -8,14 +8,15 @@ H = np.repeat(1,20)
 
 points = np.transpose(np.array(list(map(lambda x: list(x),list(zip(X,Y,Z,H))))))
 
-SE3 = Generator.generate_random_se3(-1, 1, 4)
+SE3 = Generator.generate_random_se3(-1, 1, 4, 0, 0, 0)
 
 rotated_points = np.matmul(SE3,points)
 
 (X_new,Y_new,Z_new) = list(NumUtils.points_into_components(rotated_points))
 
 #Plot3D.scatter_plot(X_new,Y_new,Z_new)
-Plot3D.scatter_plot([(X,Y,Z),(X_new,Y_new,Z_new)],['original','perturbed'])
+Plot3D.scatter_plot_array([(X,Y,Z),(X_new,Y_new,Z_new)],['original','perturbed'])
+#Plot3D.plot_array_lines([(X,Y,Z),(X_new,Y_new,Z_new)])
 
 
 

VisualOdometry/GaussNewtonRoutines.py
@@ -40,13 +40,13 @@ def compute_residual(width, height, target_index_projections, valid_measurements
             v[flat_index][0] = 0
             if not 0 < y_index < height or not 0 < x_index < width:
                 valid_measurements[flat_index] = False
-                v[flat_index][0] = -1
+                v[flat_index][0] = 0
                 continue
             # A newer SE3 estimate might re-validate a sample / pixel
             valid_measurements[flat_index] = True
             x_target = math.floor(x_index)
             y_target = math.floor(y_index)
-            error = math.fabs(target_image[y_target, x_target] - reference_image[y, x])
+            error = target_image[y_target, x_target] - reference_image[y, x]
             error_sq = error*error
             v[flat_index][0] = error
             v_sum += error_sq
@@ -58,7 +58,8 @@ def compute_residual(width, height, target_index_projections, valid_measurements
 
     end = time.time()
     #print('Runtime for Compute Residual:', end-start)
-    return v_sum
+    #return v_sum
+    return v
 
 
 def gauss_newton_step(width, height, valid_measurements,W, J_pi, J_lie, target_image_grad_x, target_image_grad_y, v,
@@ -76,7 +77,7 @@ def gauss_newton_step(width, height, valid_measurements,W, J_pi, J_lie, target_i
             J_pi_lie = np.matmul(J_pi_element, J_lie_element)
             J_full = np.matmul(J_image, J_pi_lie)
             J_t = np.transpose(J_full)
-            W_i = W[flat_index,flat_index]
+            W_i = W[0,flat_index]
             #W_J_full = np.matmul(W,J_full)
             #J_t_W = np.matmul(J_t,W)
             error_sample = v[flat_index][0]
@@ -121,7 +122,12 @@ def generate_weight_matrix(W, v, variance, degrees_of_freedom, N):
         t = v_i/variance
         t_sq = t*t
         frac = (t_sq/variance)
-        W[i,i] = numerator / (5 + frac)
+        W[0,i] = numerator / (5 + frac)
+
+def multiply_v_by_diagonal_matrix(W,v,N,valid_measurements):
+    for i in range(0,N):
+        if valid_measurements[i]:
+            v[i] *= W[0,i]
 
 
 

VisualOdometry/Solver.py
@@ -106,7 +106,14 @@ def solve_SE3(X, Y, max_its, eps):
     return SE_3_est
 
 
-def solve_photometric(frame_reference, frame_target, max_its, eps, alpha_step, use_ndc = False, debug = False):
+def solve_photometric(frame_reference,
+                      frame_target,
+                      max_its,
+                      eps,
+                      alpha_step,
+                      use_ndc = False,
+                      use_robust = False,
+                      debug = False):
     # init
     # array for twist values x, y, z, roll, pitch, yaw
     t_est = np.array([0, 0, 0], dtype=matrix_data_type).reshape((3, 1))
@@ -122,9 +129,15 @@ def solve_photometric(frame_reference, frame_target, max_its, eps, alpha_step, u
     twist_size = 6
     stacked_obs_size = position_vector_size * N
     homogeneous_se3_padding = Utils.homogenous_for_SE3()
+    variance = -1
     # Step Factor
     #alpha = 0.125
-    Gradient_step_manager = GradientStepManager.GradientStepManager(alpha_start = alpha_step, alpha_min = -0.7, alpha_step = -0.01 , alpha_change_rate = 0, gradient_monitoring_window_start = 3, gradient_monitoring_window_size = 0)
+    Gradient_step_manager = GradientStepManager.GradientStepManager(alpha_start = alpha_step,
+                                                                    alpha_min = -0.7,
+                                                                    alpha_step = -0.01 ,
+                                                                    alpha_change_rate = 0,
+                                                                    gradient_monitoring_window_start = 3,
+                                                                    gradient_monitoring_window_size = 0)
     v_mean = -10000
     v_mean_abs = -10000
     it = -1
@@ -188,7 +201,7 @@ def solve_photometric(frame_reference, frame_target, max_its, eps, alpha_step, u
         J_v = np.zeros((twist_size, 1))
         normal_matrix = np.zeros((twist_size, twist_size))
         v = np.zeros((N,1), dtype=matrix_data_type,order='F')
-        W = np.identity(N, dtype=matrix_data_type)
+        W = np.ones((1,N), dtype=matrix_data_type,order='F')
 
         # Warp with the current SE3 estimate
         Y_est = np.matmul(SE_3_est, X_back_projection)
@@ -196,7 +209,7 @@ def solve_photometric(frame_reference, frame_target, max_its, eps, alpha_step, u
 
         target_index_projections = frame_target.camera.apply_perspective_pipeline(Y_est)
 
-        v_sum = GaussNewtonRoutines.compute_residual(width,
+        v = GaussNewtonRoutines.compute_residual(width,
                                                  height,
                                                  target_index_projections,
                                                  valid_measurements,
@@ -207,7 +220,17 @@ def solve_photometric(frame_reference, frame_target, max_its, eps, alpha_step, u
 
         number_of_valid_measurements = np.sum(valid_measurements_reference)
 
+        if use_robust:
+            variance = GaussNewtonRoutines.compute_t_dist_variance(v, degrees_of_freedom, N, valid_measurements, number_of_valid_measurements, variance_min=1000, eps=0.0001)
+            if variance > 0.0:
+                GaussNewtonRoutines.generate_weight_matrix(W, v, variance, degrees_of_freedom, N)
+
+
         Gradient_step_manager.save_previous_mean_error(v_mean_abs,it)
+
+        GaussNewtonRoutines.multiply_v_by_diagonal_matrix(W,v,N,valid_measurements)
+
+        v_sum = np.matmul(np.transpose(v),v)[0][0]
 
         v_mean = v_sum / number_of_valid_measurements
         valid_pixel_ratio = number_of_valid_measurements / N
@@ -227,9 +250,6 @@ def solve_photometric(frame_reference, frame_target, max_its, eps, alpha_step, u
         #if(v_mean > Gradient_step_manager.last_error_mean_abs):
             #continue
 
-        variance = GaussNewtonRoutines.compute_t_dist_variance(v, degrees_of_freedom, N, valid_measurements, number_of_valid_measurements, variance_min=1000, eps=0.0001)
-        if variance > 0.0:
-            GaussNewtonRoutines.generate_weight_matrix(W, v, variance, degrees_of_freedom, N)
 
         GaussNewtonRoutines.gauss_newton_step(width,
                                           height,